a5ad43ee85d3ea55ae1bf203112d96f8902afae5
Nate-Castro <Nate.castro@ucas-edu.net>
2024-09-12 10:21:28 -0600

    IT WOKREDDDDD

--- whoAreYou/main.py
+++ whoAreYou/main.py
@@ -1,5 +1,8 @@
 #Nathaniel Castro :Who are you?
 
 name = input("what is your name?: ")
+age = input("What is you age?: ")
+color = input("What is your favorite color?")
 
-print("Hello", name)
+print("Hello", name, ", you are", age, ", and your favorite color is", color)
+
