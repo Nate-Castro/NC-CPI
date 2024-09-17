cee1d22c2d80029c637aca7168ebd0d0dcac266d
Nate-Castro <Nate.castro@ucas-edu.net>
2024-09-17 10:48:50 -0600

    Testy ttingy

--- ProficiencyTest/main.py
+++ ProficiencyTest/main.py
@@ -1,2 +1,6 @@
 #Nathaniel Castro ProficiencyTest: Celsius to Fahrenheit
 
+celsius = input("What is the temperature?")
+
+fahrenheit = (int(celsius)* 9/5)+32
+print("In fahrenheit it is", fahrenheit, ", In Celsius it is", celsius)
