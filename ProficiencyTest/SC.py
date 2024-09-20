e607fb7bc7a7030b50b78e07823036fba1c4791e
Nate-Castro <Nate.castro@ucas-edu.net>
2024-09-20 10:26:19 -0600

    CuhqLaytour

--- ProficiencyTest/SC.py
+++ ProficiencyTest/SC.py
@@ -1,4 +1,11 @@
 #Nathaniel Castro ProfTest: Simple Calculator
 
-
-
+x = int(input("What is x?: "))
+y = int(input("What is y?: "))
+print("When added it is", x + y) 
+print("When subtracted it is", x - y)
+print("When multiplied it is", x * y)
+print("When divded it is", x / y)
+print("Showing its remainder is", x % y)
+print("When showing it with exponets it is", x ** y )
+print("When using floor division it is", x // y)
