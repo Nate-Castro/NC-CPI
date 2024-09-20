b6ca59d1d4fe23d28b4f27d68deef1a36ebd9457
Nate-Castro <Nate.castro@ucas-edu.net>
2024-09-20 10:50:28 -0600

    Devin

--- ProficiencyTest/Madlib.py
+++ ProficiencyTest/Madlib.py
@@ -1,3 +1,7 @@
 #Nathaniel Castro ProfTest: Madlib
-
-
+noun = input("Put a noun: ")
+verb = input("Put a verb: ")
+color = input("Put a color: ")
+furniture = input("Name a type of furniture: ")
+run = ("Put down a word for happy")
+print("Devin woke up and decided to take his", noun,"on a ", verb, ". Whilst they were going merrily on their",verb, " they had a sudden incounter with a huge,", color, "Monster. Devin knew what he must do and threw a ", furniture, "At the monster. He then went into the moon", run, "with his", noun )
