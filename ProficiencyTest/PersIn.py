341a942a1f2fe03f2cb38a7aae15846e3ae3f8b2
Nate-Castro <Nate.castro@ucas-edu.net>
2024-09-25 10:28:25 -0600

    YAYYYYYYY I DEED IT

--- ProficiencyTest/PersIn.py
+++ ProficiencyTest/PersIn.py
@@ -1,2 +1,11 @@
 # Nathaniel Castro ProfTest: Personal information converter
 
+x = input("What is your name?: ")
+y = input("How old are you?: ")
+z = input("How tall are you in meters?: ")
+a = input("What is your favorite number?: ")
+print("Cool, your name is ", x, ",You are ", y, "Years of age. You are ", z, " meters tall and your favorite number is ", a, ".")
+y = int(y)
+a = int(a)
+z = float(z)
+print("Cool, your name is ", x, ",You are ", y, "Years of age. You are ", z, " meters tall and your favorite number is ", a, ".")
