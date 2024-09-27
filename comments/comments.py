3a9cb939f352ea4b36d849fb94bc60c7ba2fb6ba
Nate-Castro <Nate.castro@ucas-edu.net>
2024-09-27 10:43:41 -0600

    sdf

---
+++ comments/comments.py
@@ -0,0 +1,4 @@
+#Nathaniel Castro OtherVideo: Python Comments
+
+name = input("What is your name?: ")#This asks the user to put in their name 
+print("Hello ",name)#This speaks to the user saying hello and whatever they put in as name
