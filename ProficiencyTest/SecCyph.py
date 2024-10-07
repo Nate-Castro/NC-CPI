265a4544a934b3c1f712954202df905aadbfcac6
Nate-Castro <Nate.castro@ucas-edu.net>
2024-10-07 10:51:48 -0600

    qwdq

---
+++ ProficiencyTest/SecCyph.py
@@ -0,0 +1,6 @@
+#Nathaniel Castro ProfTest: Secret Cypher
+
+string1 = input("Give your Shift Cypher:")
+
+def decode():
+    
---
+++ comments/sd.py
@@ -0,0 +1,5 @@
+def greet(num_1, num_2):
+    return(num_1 - num_2)
+
+print(int(greet(1, 1)))
+print(int(greet(743, 834)))
