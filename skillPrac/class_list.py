ce720c4727c437cabe3f4758d734bff98f5e5b90
Nate-Castro <Nate.castro@ucas-edu.net>
2024-10-22 10:12:41 -0600

    wqqwd

---
+++ skillPrac/class_list.py
@@ -0,0 +1,12 @@
+#Nathaniel Castro SkillPrac: List of classes
+
+classes = ["Geography", "CS 1400", "Biology", "Lunch", "Advisory", "CSP", "English", "Math"]
+
+print("My first period is",classes[0])
+print("My second period is",classes[1])
+print("My third period is",classes[2])
+print("My fourth period is",classes[3])
+print("My fifth period is",classes[4])
+print("My sixth period is",classes[5])
+print("My seventh period is",classes[6])
+print("My eighth is",classes[7])
