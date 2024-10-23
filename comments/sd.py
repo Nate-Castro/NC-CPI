9519debe07b01f2fb031e5d48337d6f233c3bc20
Nate-Castro <Nate.castro@ucas-edu.net>
2024-10-23 10:51:06 -0600

    asdasd

--- comments/sd.py
+++ comments/sd.py
@@ -1,15 +1,2 @@
-def sum_even_numbers(numbers):
 
-     sum = 0
 
-     for num in numbers:
-          
-          if num % 2 == 0:
-
-               sum += num
-
-          else:
-
-               return 0
-          
-     return sum
