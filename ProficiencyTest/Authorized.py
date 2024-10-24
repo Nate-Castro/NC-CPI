c18c3fef77f3068275c0ba66250c85d4372ff8e7
Nate-Castro <Nate.castro@ucas-edu.net>
2024-10-24 10:50:47 -0600

    asdsad

--- ProficiencyTest/Authorized.py
+++ ProficiencyTest/Authorized.py
@@ -4,7 +4,12 @@ auth_user = ["Nate", "Devin", "Gavin", "Royal", "William", "Matthew", "Locklin",
 user = input("What is your name: ")
 
 
+
 if user in auth_user:
     print("You are allowed to come in")
+
+if user == "Nate":
+    print("Hello Admin")
+
 else:
     print("No entry")
