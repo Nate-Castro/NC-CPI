da5615c12910ec5508c2d26e03a25094e79b75c3
Nate-Castro <Nate.castro@ucas-edu.net>
2024-10-28 10:38:41 -0600

    xas

---
+++ ProficiencyTest/rock_paper_scissor.py
@@ -0,0 +1,8 @@
+#Nathaniel Castro ProfTest: Rock, Paper, Scissors
+
+play = input("Would you like to play rock, paper, scissors?: ")
+if play == "no":
+    print("Okay, bye bye")
+
+if play == "yes":
+    print("Okay")
