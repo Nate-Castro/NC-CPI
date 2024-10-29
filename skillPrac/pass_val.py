48162c10fda10db800e84e8616f69d6bce61bd12
nate-castro <nate.castro@ucas-edu.net>
2024-10-29 10:45:08 -0600

    sa

--- skillPrac/pass_val.py
+++ skillPrac/pass_val.py
@@ -1,2 +1,8 @@
 #Nathaniel Castro SkillPrac: Password Validator
 
+password = input("Give a password: ")
+x = ("@", "#", "$", "%", "*", "&")
+
+while x not in password:
+    print("Try again and it must contain", x)
+    input("Give new password")
