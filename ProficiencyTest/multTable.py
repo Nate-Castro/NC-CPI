a0ea5fbe1825d63fa8f17058869a8b7f3ff1874e
nate-castro <nate.castro@ucas-edu.net>
2024-10-29 10:39:04 -0600

    as

---
+++ ProficiencyTest/multTable.py
@@ -0,0 +1,6 @@
+#Nathaniel Castro ProfTest: Multiplication Table
+
+num = int(input("Give a number that you want multiples of: "))
+
+table = 144
+print(table)
--- skillPrac/variable.py
+++ skillPrac/variable.py
@@ -3,4 +3,12 @@
 staff = 32
 students = 100
 table = 12
+guests = 200
 
+students = 99
+guests = 198
+staff = 29
+
+guests = 183
+
+print(int(students + guests + staff))
