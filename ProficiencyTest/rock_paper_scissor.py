835e8c7333927c22d43f35c8e0613822502ba3e1
nate-castro <nate.castro@ucas-edu.net>
2024-10-31 10:50:18 -0600

    asds

--- ProficiencyTest/rock_paper_scissor.py
+++ ProficiencyTest/rock_paper_scissor.py
@@ -1,10 +1,32 @@
 #Nathaniel Castro ProfTest: Rock, Paper, Scissors
 
+import random
+score =+ 1
+
+com1 = ["rock", "paper", "scissors"]
+
+#print(random.choice(com1))
+
 play = input("Would you like to play rock, paper, scissors?: ")
 if play == "no":
     print("Okay, bye bye")
 
 if play == "yes":
     print("Okay")
-    while True:
-        print("You go first")
+    
+    print("You go first")
+
+user1 = input("Rock, paper, or scissors?: ")
+print(random.choice(com1))
+if user1 == "scissors" and com1 == "rock":
+    print("Computer wins!")
+
+elif user1 == "rock" and com1 == "scissors":
+    print("User wins!")
+
+elif user1 == "rock" and com1 == "paper":
+    print("Computer wins!")
+
+elif user1 == "paper" and com1 == "rock":
+    print("User wins!")
+
