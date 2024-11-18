1894f26d5d0eff108138d1367350ff2556bb27dd
nate-castro <nate.castro@ucas-edu.net>
2024-11-18 10:50:49 -0700

    asddsa

---
+++ ProficiencyTest/SQG.py
@@ -0,0 +1,8 @@
+#Nathaniel Castro ProfTest: Simple Quiz Game
+
+print("Welcome to The Quiz!\n", "Lets see how much you know about Math")
+
+answer1 = int(input("What is 25 X 2?: "))
+
+if answer1 == 50:
+    print("That is correct!")
--- comments/sd.py
+++
@@ -1,5 +0,0 @@
-def add_to_list(item, my_list=[]):
-
-     my_list.append(item)
-
-     return my_list
