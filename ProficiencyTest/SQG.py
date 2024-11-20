5355a98cc19a1554b560047d085a1a0415dd4bfd
nate-castro <nate.castro@ucas-edu.net>
2024-11-20 10:50:31 -0700

    safdsad

--- ProficiencyTest/SQG.py
+++ ProficiencyTest/SQG.py
@@ -1,10 +1,24 @@
 #Nathaniel Castro ProfTest: Simple Quiz Game
-
+score = 0
 print("Welcome to The Quiz!\n", "Lets see how much you know about Math")
 
-answer1 = int(input("What is 25 X 2?: "))
+answer1 = input("""What is the answer to 25 X 2?(Please use the letter for answer)
+                A. 27
+                B. 50
+                C. 94
+                D. 81
+                :""")
+if answer1.lower() == "b":
+    print("Correct")
+    score += 1
+    print("This is your score currently", score)
+if answer1 != "b":
+    print("Wrong")
+    score +=0 
+    print("This is your score currently", score)
+    while answer1 != "b":
+        wrong1 = input("""What is the name of the final boss in Hollow Knight?
+                       A. Moss Kight
+                       B. Bee Kinght
+                       C. The Hollow Knight""")
 
-if answer1 == 50:
-    print("That is correct!")
-    if answer1 != 50:
-        print("Wrong answer")
