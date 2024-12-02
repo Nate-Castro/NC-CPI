ff1ccddc5f56e9dfcc140a5a710cea4d33e315dc
nate-castro <nate.castro@ucas-edu.net>
2024-12-02 10:50:34 -0700

    qawqd

--- ProficiencyTest/Ti-Ta-To.py
+++ ProficiencyTest/Ti-Ta-To.py
@@ -1,17 +1,11 @@
 #Nathaniel Castro ProfTest: Tic-Tac-Toe Game
-
-print("Welcome to you tic-tac-toe game! You are X and the computer is O.")
-user = input("""Where do you want to place?
-             A. Top right
-             B. Middle top
-             C. Top left
-             D. middle left
-             E. Middle""")
+import random
+print("Welcome to you tic-tac-toe game! You are X and the computer is O, Here is the board.")
 
 
 
-column1 = ["       |             |", "       |             |", "       |             |"]
-row1 = ["____________________________"] 
+column1 = ["    |      |", "    |      |", "    |      |"]
+row1 = ["_________________"] 
 b = 2
 q = 2
 f = 2
@@ -31,3 +25,14 @@ while f < 3:
     f += 1
     for x in column1:
         print(x)
+user = input("""Where do you want to place?
+             A. Top right
+             B. Top middle
+             C. Top left
+             D. Middle left
+             E. Middle
+             F. Middle righ5t
+             G. Bottom left
+             H. Bottom middle
+             I. Bottom right
+             : """)
--- comments/sd.py
+++ comments/sd.py
@@ -1,4 +1,6 @@
 
 
-for x in range(1, 10):
-    print(x, end="")
+grid = [[1,2,3],
+[4,5,6],
+[7,8,9]]
+print(grid)
