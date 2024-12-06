26e2b7fd846272c93b3b20e894ac5faf46be0e38
nate-castro <nate.castro@ucas-edu.net>
2024-12-06 10:35:27 -0700

    adsasd

--- ProficiencyTest/Ti-Ta-To.py
+++ ProficiencyTest/Ti-Ta-To.py
@@ -12,7 +12,7 @@ com1 = ["1","2","3","4","5","6","7","8","9"]
 user_choices = ["1","2","3","4","5","6","7","8","9"]
 
 
-if grid[0] == "x" and grid[0] == grid[1] and grid[1] == grid[2]:
+if [0] == "x" and grid[0] == grid[1] and grid[1] == grid[2]:
     print("You win")
 
 
@@ -30,37 +30,27 @@ if start == "yes":
             if user not in user_choices:
                  print("You cannot place here.")
                  continue
-            user_choices.remove(user)
-                
-            if grid[0]==
-            if user == enumerate(grid):
-                        print("you need a valid space")
-                        user=int(input("Where do you want to put your X? (Use number placement in table): "))
-            if user == enumerate(grid):
-                        print("you need to chose a empty space")
-                        user=int(input("Where do you want to put your X? (Use number placement in table): "))    
-            
+            user_choices[(int(user)) - 1] = "x"
+
+
 
             for rownum, row in enumerate(grid):
                 for placenum, place in enumerate(row):
                     if user == place:
                         grid[rownum][placenum] = "x"
-
-                    
-                
-     #               while user == "x":
-      #                  print("you need a valid space")
-       #                 user=int(input("Where do you want to put your X? (Use number placement in table): "))
-        #            while user == "o":
-         #               print("you need to chose a empty space")
-          #              user=int(input("Where do you want to put your X? (Use number placement in table): "))    
+                        
+                     
             print("Here is the board")
             for row in grid:
                 print(row)
 
+            if user_choices[0] == user_choices[1] and user_choices[1] == user_choices[2]:
+                print("You win")
+                break
+
             com_choice = random.choice(com1)
             if com_choice not in user_choices:
-                 continue
+                continue
             user_choices.remove(com_choice)
 
             for rownum, row in enumerate(grid):
